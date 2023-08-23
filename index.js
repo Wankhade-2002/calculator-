2e71ecd786e1c67b49216213f0b35e1cf764909e
Rohit <rohitwankhade6337@gmail.com>
2023-08-23 23:02:07 +0530

    Changes saved successfully

--- index.js
+++
@@ -1,9 +0,0 @@
-let a = document.querySelectorAll('.btn')
- for (let i = 0; i < a.length; i++) {
-    // a[i] = array[i];
-    a[i].addEventListener("click", function (e) {
-        const tgt = e.target
-        console.log(tgt)
-    
- })
-}
